93b9bf365e67d271351611441a4b8a1992121cd8
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2021-10-18 14:00:00 +0300

    Proje dosyası ekle.

---
+++ js/myJsFile.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function ekranaYazdir() {
+    document.write("sayfa içine buyazý yazýlacak");
+    // ekrana(body içine) yazdýrmak için kullanýlýr.
+
+    /*birden
+     fazla 
+     yorum
+     satýrý*/
+}
